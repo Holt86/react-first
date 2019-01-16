c6f31720e05f9067ca9fdf86ae5819fcd6f5283b
A.Ovechnikov <ayovechnikov.gmail.com>
2019-01-16 17:15:35 +0300

    fix classes

--- src/Components/TodoList/TodoListTaskCreator.js
+++ src/Components/TodoList/TodoListTaskCreator.js
@@ -1,40 +1,70 @@
-import React, { Component } from 'react';
+import React from 'react';
 //import './TodoList.css';
 import {createTaskOnServer} from './Services.js'
 
-class TodoListTaskCreator extends Component {
+const TodoListTaskCreator = (props) =>{
 
-    constructor(props) {
-        super(props);
-    }
-
-    createNewTask(event) {
+    const createNewTask = (event) => {
         if (event.key === 'Enter') {
 
             const inputTarget = event.currentTarget;
 
-          createTaskOnServer(inputTarget.value, 123)
+            createTaskOnServer(inputTarget.value, 123)
                 .then(result => {
                     const newTask =
                     {
-                      id: result.task.id,
-                      title: result.task.title,
-                      isDone: result.task.done
-                    }
-                    this.props.onCreate(newTask);
+                        id: result.task.id,
+                        title: result.task.title,
+                        isDone: result.task.done
+                    };
+                    props.onCreate(newTask);
                     inputTarget.value='';
                 });
         }
-    }
+    };
+    return (
+        <div className="header">
+            <input onKeyPress={createNewTask}/>
+        </div>
 
-    render() {
-        return (
-            <div className="header">
-                <input onKeyPress={this.createNewTask.bind(this)}/>
-            </div>
-
-        );
-    }
-}
+    );
+};
 
 export default TodoListTaskCreator;
+//
+//class Blabla extends Component {
+//
+//    constructor(props) {
+//        super(props);
+//    }
+//
+//    createNewTask(event) {
+//        if (event.key === 'Enter') {
+//
+//            const inputTarget = event.currentTarget;
+//
+//          createTaskOnServer(inputTarget.value, 123)
+//                .then(result => {
+//                    const newTask =
+//                    {
+//                      id: result.task.id,
+//                      title: result.task.title,
+//                      isDone: result.task.done
+//                    }
+//                    this.props.onCreate(newTask);
+//                    inputTarget.value='';
+//                });
+//        }
+//    }
+//
+//    render() {
+//        return (
+//            <div className="header">
+//                <input onKeyPress={this.createNewTask.bind(this)}/>
+//            </div>
+//
+//        );
+//    }
+//}
+//
+//export default TodoListTaskCreator;
